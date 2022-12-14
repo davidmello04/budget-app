872939d0ef32d912d692d221e7a65ec623ebe072
David Mello <davidbezerra10@hotmail.com>
2022-12-14 13:36:24 -0300

    Página inicial e consulta.
    Criei a página inicial e a página consulta.
    Adicionei a funcionalidade no botão + para obter os valores digitados para criação do objeto com as informações.

---
+++ app.js
@@ -0,0 +1,34 @@
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
+        this.ano = ano
+        this.mes = mes
+        this.dia = dia
+        this.tipo = tipo
+        this.descricao = descricao
+        this.valor = valor
+    }
+}
+
+function cadastrarDespesa() {
+    let ano = document.getElementById("ano");
+    let mes = document.getElementById("mes");
+    let dia = document.getElementById("dia");
+    let tipo = document.getElementById("tipo");
+    let descricao = document.getElementById("descricao");
+    let valor = document.getElementById("valor");
+
+    let despesa = new Despesa(
+        ano.value,
+        mes.value,
+        dia.value,
+        tipo.value,
+        descricao.value,
+        valor.value
+    );
+
+    gravar(despesa)
+}
+
+function gravar(d) {
+    localStorage.setItem('despesa', JSON.stringify(d))
+}
